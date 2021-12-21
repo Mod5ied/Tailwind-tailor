de16104055cf52238ef625bb6955f1379016d99f
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2021-12-21 15:21:52 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        lighter: "#fcfcfc",
+        light: "#eeeeee",
+        secondary: {
+          100: "#fcfcfce3",
+          200: "#64646454"
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
